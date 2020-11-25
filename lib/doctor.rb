8a03e803acc1b28c8c7034ff6d5a58e7375f180a
Wesley <wes.beck21@outlook.com>
2020-11-24 17:42:28 -0800

    commit tree da39eb68b2c6a3eb4d2f00abc5ebfe8bd3c10244

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+class Doctor
 
+    attr_accessor :name, :patient, :date
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.map do |appt|
+            appt.patient
+        end
+    end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
+    attr_accessor :name, :date, :doctor
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.patient == self
+        end
+    end
+
+    def doctors
+        appointments.map do |appt|
+            appt.doctor
+        end
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+end
